d08eb42615aeb5452515811fc95acfeb13b3c43d
robwirving <rob.irving@gmail.com>
2014-05-17 15:59:16 -0400

    Initial Commit
    Sample project for target

---
+++ PhoneApp_SL_8.05/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneApp_SL_8._05.Resources;
+
+namespace PhoneApp_SL_8._05
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
